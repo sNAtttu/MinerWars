420f045ad967766a5099e35e626d96dfc5e78468
sNAtttu <nsanteri@hotmail.com>
2015-11-21 16:09:59 +0200

    refactored map initialization

--- main.js
+++ main.js
@@ -10,15 +10,49 @@
     var heightSquares = mapHeight / 32;
 
     InitMapArray(mapArray, widthSquares, heightSquares);
+    DrawLand(mapArray, context);
+    DrawTreasures(mapArray, context);
+}
 
+function DrawLand(map,context) {
     var grass = new Image();
-    var sand = new Image();
     grass.src = "Graphics/Tilesets/grass.png";
+    var posX = 0;
+    var posY = 0;
+    grass.onload = function () {
+        for (var i = 0; i < map.length; i++) {
+            for (var j = 0; j < map[i].length; j++) {
+
+                if (map[i][j] == 0) {
+                    context.drawImage(grass, posX, posY, 32, 32);
+                }
+                posX += 32;
+
+            }
+            posX = 0;
+            posY += 32;
+        }
+    }
+}
+
+function DrawTreasures(map, context) {
+    var sand = new Image();
     sand.src = "Graphics/Tilesets/sand.png";
+    var posX = 0;
+    var posY = 0;
+    sand.onload = function () {
+        for (var i = 0; i < map.length; i++) {
+            for (var j = 0; j < map[i].length; j++) {
 
-    grass.onload = drawGrass(context, mapArray, grass);
-    sand.onload = drawSand(context, mapArray, sand);
-
+                if (map[i][j] == 1) {
+                    context.drawImage(sand, posX, posY, 32, 32);
+                }
+                posX += 32;
+            }
+            posX = 0;
+            posY += 32;
+        }
+    }
 }
 
 function InitMapArray(mapArray, xSquares, ySquares) {
@@ -29,37 +63,5 @@ function InitMapArray(mapArray, xSquares, ySquares) {
         for (var j = 0; j < xSquares; j++) {
             mapArray[i][j] = 0;
         }
-    }
-}
-
-function drawSand(context, map, image, posX, posY) {
-    var posX = 0;
-    var posY = 0;
-    for (var i = 0; i < map.length; i++) {
-        for (var j = 0; j < map[i].length; j++) {
-
-            if (map[i][j] == 1) {
-                context.drawImage(image, posX, posY, 32, 32);
-            }
-            posX += 32;
-        }
-        posX = 0;
-        posY += 32;
-    }
-}
-
-function drawGrass(context, map, image, posX, posY) {
-    var posX = 0;
-    var posY = 0;
-    for (var i = 0; i < map.length; i++) {
-        for (var j = 0; j < map[i].length; j++) {
-
-            if (map[i][j] == 0) {
-                context.drawImage(image, posX, posY, 32, 32);
-            }
-            posX += 32;
-        }
-        posX = 0;
-        posY += 32;
     }
 }
