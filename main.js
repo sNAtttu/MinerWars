87849f50cbf615c175ebd52fecc8f962609ca4ab
sNAtttu <nsanteri@hotmail.com>
2015-11-21 13:53:45 +0200

    deleted framework elements

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-﻿RPGJS.defines({
-    canvas: "mainArea",
-    autoload: false
-}).ready(function () {
+﻿function initMap() {
+    var canvas = document.getElementById('mainArea');
+    var context = canvas.getContext('2d');
 
-    RPGJS.Scene.map();
+    var mapArray = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
 
-});
+    var grass = new Image();
+    var sand = new Image();
+
+    grass.src = "Graphics/Tilesets/grass.png";
+    sand.src = "Graphics/Tilesets/sand.png";
+}
+initMap();
